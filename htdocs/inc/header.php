5f3e7fa88c86d71436f1c004b13d8263d21e02ee
alimd <ali@mihandoost.com>
2012-07-18 09:10:07 +0430

    add owa to all page

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -5,6 +5,7 @@
 	<title>Google Chrome Frame</title>
 	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
 	<link href="style.css" rel="stylesheet" type="text/css" />
+<?php include 'inc/owa.php'; ?>
 </head>
 <body>
 	<div id="container">

0bee4eb533442028ef42d1dd1ebb8cdaaf83bb8f
alimd <ali@mihandoost.com>
2012-07-18 08:37:58 +0430

    validate html @aliab mage dastam behet narese

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,26 +1,24 @@
 <!doctype html>
-<html>
+<html lang="fa-IR" dir="rtl">
 <head>
-<meta charset="utf-8">
+<meta charset="utf-8" />
 <title>Google Chrome Frame</title>
-<link href="style.css" rel="stylesheet" type="text/css">
-<link href="images/chrome-16.png" rel="icon" type="image/ico">
-
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="images/chrome-16.png" rel="icon" type="image/ico" />
 </head>
 <body>
 <div id="container">
 	<div id="header" class="section">
-
-	<div id="logo">
-		<a href="http://chromeframe.ir"><img src="images/chrome_logo.jpg" width="179" height="40" style="border:0;" /></a>
+		<div id="logo">
+			<a href="http://chromeframe.ir"><img src="images/chrome_logo.jpg" alt="Google Chrome" width="179" height="40" style="border:0;" /></a>
+		</div>
+		<ul id="navigation-bar">
+			<li class="active"><a href="index.html"><span>درباره گوگل فریم</span></a></li>
+			<li><a href="developers.html"><span>توسعه دهنده ها</span></a></li>
+			<li><a href="basic.html"><span>سوالات متداول</span></a></li>
+		</ul>
+		<div class="clear"></div>
 	</div>
-	
-	<ul id="navigation-bar">
-		<li class="active"><a href="index.html"><span>درباره گوگل فریم</span></a></li>
-		<li><a href="developers.html"><span>توسعه دهنده ها</span></a></li>
-		<li><a href="basic.html"><span>سوالات متداول</span></a></li>
-	</ul>
-	<div class="clear"></div>
 	<div id="content">
 		<div id="description">
 			<h1> وارد دنیایی جدید از وب شوید! </h1>
@@ -29,21 +27,21 @@
 			<p>برای ویندوز 7/Vista/XP</p>
 		</div>
 		<div id="des-image">
-			<img src="images/browser-win.jpg" width="928" height="266" />
+			<img src="images/browser-win.jpg" alt="Google Chrome Frame" width="928" height="266" />
 		</div>
 		<div id="sub-description">
 			<div class="section-down">
 				<div class="sec-content">
 					<h2> اینترنت اکسپلورر جادویی </h2>
 					<p>تکنولوژی های وب را به اینترنت اکسپلورر بیاورید، کروم فریم به مرورگر پیشفرض ویندوز امکان استفاده از تکنولوژی های روز را می دهد</p>
-					<p class="more"> <a href="basic.html"> اطلاعات بیشتر </a> </p>
+					<p class="more"><a href="basic.html"> اطلاعات بیشتر </a></p>
 				</div>
 			</div>
 			<div class="section-down">
 				<div class="sec-content">
 					<h2> برای توسعه دهنده ها </h2>
 					<p> استفاده از کروم فریم برای شما سریع و ساده است، کافی است با یک تگ اینترنت اکسپلورر را مجاب به استفاده از کروم فریم کنید. </p>
-					<p class="more"> <a href="developers.html"> اطلاعات بیشتر </a> </p>
+					<p class="more"><a href="developers.html"> اطلاعات بیشتر </a></p>
 				</div>
 			</div>
 			<div class="section-down">
@@ -57,21 +55,21 @@
 			<div class="clear"></div>
 		</div>
 	</div>
-	<div id="footer">
-		<div id="fotter-content">
-			<div id="sub-footer">
-				<div id="sub-footer-left">
-					<p>&copy;2012 Google</p>
-					<a href="http://www.google.com/policies/privacy/">Privacy &amp; Terms</a>
-					<a href="https://www.google.com/intl/en/chrome/browser/?hl=en">Google Chrome</a>
-					<a href="basic.html">Help.</a>
-				</div>
-				<div id="sub-footer-right">
-					<a>With some iranian friends on 1Devs inc</a>
-				</div>
+</div>
+<div id="footer">
+	<div id="fotter-content">
+		<div id="sub-footer">
+			<div id="sub-footer-left">
+				<p>&copy;2012 Google</p>
+				<a href="http://www.google.com/policies/privacy/">Privacy &amp; Terms</a>
+				<a href="https://www.google.com/intl/en/chrome/browser/?hl=en">Google Chrome</a>
+				<a href="basic.html">Help.</a>
 			</div>
-		</div>   
-	</div>
+			<div id="sub-footer-right">
+				<a>Made with some friends at 1Devs inc.</a>
+			</div>
+		</div>
+	</div>   
 </div>
 </body>
 </html>
